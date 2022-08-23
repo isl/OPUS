efe94988ab0793f0ba41981d557a81f389cbebfe
troulin <59057867+troulin@users.noreply.github.com>
2022-08-23 12:15:23 +0300

    Delete run_blap.sh

--- scripts/run_blap.sh
+++
@@ -1,7 +0,0 @@
-spark-submit \
---class org.ics.isl.BLAP \
---driver-memory DRIVER_MEM \
---executor-memory EXECUTOR_MEM \
---conf spark.speculation=true \
---master MASTER \
-PATH_TO_JAR $1 $2 $3 $4 $5
